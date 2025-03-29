1d3dabd1f5ca7e20c55f8d0010d575445a9d1faf
elen-afipe <190838117+elen-afipe@users.noreply.github.com>
2025-03-29 15:48:58 +0300

    add new form fields

--- src/lib/components/form/form.tsx
+++ src/lib/components/form/form.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 import styles from './form.module.scss';
+import { useState } from 'react';
 
 const  SERVICE_ID = "service_karayaka" ; 
 const  TEMPLATE_ID = "template_karayaka" ; 
@@ -35,10 +36,20 @@ export const ContactUs = () => {
       );
   };
 
+  const [purposes, setPurposes] = useState({
+    Buy: false,
+    Rent: false
+  });
+
+  const [locations, setLocations] = useState({
+    Russia: false,
+    Turkey: false
+  });
+
   return (
         <form className={styles.form} ref={form} onSubmit={sendEmail}>
         <legend>Your message</legend>
-                <p>Share your contact details and property wishes in the form below. We'll get back to you with recommendations.</p>
+                <p>Share your contact details and property wishes. We'll get back to you with recommendations.</p>
 
                 <div className={styles.formRow}>
                   <div>
@@ -48,7 +59,73 @@ export const ContactUs = () => {
 
                   <div>
                     <label for="surname">Surname</label>
-                    <input type="text" id="surname" name="surname" required/>
+                    <input type="text" id="surname" name="surname"/>
+                  </div>
+                </div>
+
+                <div className={styles.formDetails}>
+                  <div className={styles.leftDetails}>
+                    <div className={styles.formLocation}>
+                      <label for="location" className={styles.buttonsLabel}>Where are you looking?</label>
+                        <div>
+                          <input 
+                              type="button" 
+                              name="location" 
+                              id="location-russia" 
+                              value="Russia"
+                              className={locations.Russia ? `${styles.input} ${styles.selected}` : styles.input}
+                              onClick={() => setLocations(prev => ({...prev, Russia: !prev.Russia}))}
+                              ></input>
+                          <input 
+                              type="button" 
+                              name="location" 
+                              id="location-turkey" 
+                              value="Turkey"
+                              className={locations.Turkey ? `${styles.input} ${styles.selected}` : styles.input}
+                              onClick={() => setLocations(prev => ({...prev, Turkey: !prev.Turkey}))}
+                              ></input>
+                        </div>
+                    </div>
+
+                    <div className={styles.formPurpose}>
+                      <label for="purpose" className={styles.buttonsLabel}>Purpose</label>
+                        <div>
+                          <input 
+                              type="button" 
+                              name="purpose" 
+                              id="purpose-buy" 
+                              value="Buy" 
+                              className={purposes.Buy ? `${styles.input} ${styles.selected}` : styles.input}
+                              onClick={() => setPurposes(prev => ({...prev, Buy: !prev.Buy}))}
+                          ></input>
+
+                          <input 
+                              type="button" 
+                              name="purpose" 
+                              id="purpose-rent"
+                              value="Rent"
+                              className={purposes.Rent ? `${styles.input} ${styles.selected}` : styles.input}
+                              onClick={() => setPurposes(prev => ({...prev, Rent: !prev.Rent}))}
+                          ></input>
+                        </div>
+                    </div>
+                  </div>
+
+                  <div className={styles.rightDetails}>
+                      <div className={styles.formRow}>
+                        <label for="city">City</label>
+                        <input type="text" id="city" name="city"/>
+                      </div>
+
+                      <div className={styles.formRow}>
+                        <label for="district">District</label>
+                        <input type="text" id="district" name="district"/>
+                      </div>
+
+                      <div className={styles.formRow}>
+                        <label for="budget">Budget</label>
+                        <input type="text" id="budget" name="budget"/>
+                      </div>
                   </div>
                 </div>
 
@@ -61,7 +138,7 @@ export const ContactUs = () => {
                   <label for="email">Email</label>
                   <input type="email" id="email" name="email" required/>
                 </div>
-                <textarea aria-label='Your message' placeholder="Describe what you're looking for: buy or rent, preferred location, budget range, must-have features. The more details you provide, the better we can help you find your perfect match!" name="message" id="message"></textarea>
+                <textarea aria-label='Your message' placeholder="Describe what you're looking for: number of rooms, floor, must-have features. The more details you provide, the better we can help you find your perfect match!" name="message" id="message"></textarea>
             <input className={styles.formBtn} type="submit" value="Send" />
         </form>
   );
