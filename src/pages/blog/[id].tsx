15f22ff244e44fbee61237db0f7420266279b8ea
sotakeiteasy <murasov96@gmail.com>
2025-03-30 19:56:52 +0300

    fix blog props

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,7 +1,11 @@
 import { getAllPostIds, getPostData } from "../../lib/blog";
 import Date from '@components/date/date'
 import styles from './id.module.scss';
-export default function Post({ postData }: {postData: any}) {
+export default function Post({ postData }: {postData: any}) {   
+    if (!postData) {
+        return <div>Loading...</div>;
+    }
+
     return (
     <main className={styles.main}>
         <section className={styles.article}>
@@ -36,12 +40,35 @@ export async function getStaticProps( { params, locale }: {params:any, locale: s
     //     }
     //   }
 
-    const postData = await getPostData(params.id);
-
-    return {
-        props: {
-            ...(await serverSideTranslations(locale, ['common'])),
-            postData
+    try {
+        const postData = await getPostData(params.id);
+        
+        // Проверка наличия данных, используя только определенные свойства
+        if (!postData || !postData.contentHtml) {
+            return {
+                notFound: true,
+            };
         }
+        
+        return {
+            props: {
+                ...(await serverSideTranslations(locale, ['common'])),
+                postData
+            }
+        }
+    } catch (error) {
+        console.error(`Error fetching post ${params.id}:`, error);
+        return {
+            notFound: true
+        };
     }
+
+    // const postData = await getPostData(params.id);
+
+    // return {
+    //     props: {
+    //         ...(await serverSideTranslations(locale, ['common'])),
+    //         postData
+    //     }
+    // }
 }
