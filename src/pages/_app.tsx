a7edfc4a1bd980633c8a97596d853348a0d4d7c9
sotakeiteasy <murasov96@gmail.com>
2025-04-08 14:40:34 +0300

    update yandex verification and metrica counter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,26 +18,26 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
       
       {/* Yandex.Metrika counter */}
-      <Script id="yandex-metrika" strategy="afterInteractive">
-        {`
+        <script type="text/javascript" >
+          {`
           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-          ym(100805346, "init", {
-               clickmap:true,
-               trackLinks:true,
-               accurateTrackBounce:true
+          ym(100868560, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
           });
-        `}
-      </Script>
-      <noscript>
-        <div>
-          <img src="https://mc.yandex.ru/watch/100805346" style={{ position: 'absolute', left: '-9999px' }} alt="" />
-        </div>
-      </noscript>
+          `}
+        </script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/100868560" style={{position: 'absolute', left: '-9999px'}} alt="" />
+          </div>
+        </noscript>
       {/* /Yandex.Metrika counter */}
       
       <Header />
