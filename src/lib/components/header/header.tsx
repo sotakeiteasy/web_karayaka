8432c25a00fc0cab471d6320c7c15a1f84f1f2d1
sotakeiteasy <murasov96@gmail.com>
2025-06-11 16:44:58 +0300

    feat(jsonld): header micro-markup

--- src/lib/components/header/header.tsx
+++ src/lib/components/header/header.tsx
@@ -1,12 +1,12 @@
 import styles from './Header.module.scss';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { useTranslation, LinkWithLocale, LanguageSwitcher } from 'next-export-i18n';
-import { useLanguageQuery } from 'next-export-i18n';
+import { useLanguageQuery, useTranslation, LinkWithLocale, LanguageSwitcher } from 'next-export-i18n';
 import Icon from '@mdi/react';
 import { mdiTriangleSmallDown, mdiWhatsapp, mdiClose } from '@mdi/js';
 import { useState, useEffect, useRef } from 'react';
 import { contactInfo } from '@/lib/constants/contactInfo';
+import Head from 'next/head';
 
 export function Header() {
   const { t } = useTranslation();
@@ -65,105 +65,130 @@ export function Header() {
   ];
 
   return (
-    <header className={styles.header}>
-      <div
-        className={`${styles.overlay} ${isBurgerMenuOpen ? styles.active : ''}`}
-        onClick={() => setIsBurgerMenuOpen(false)}
-      ></div>
+    <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'ItemList',
+              'itemListElement': [
+                {
+                  '@type': 'SiteNavigationElement',
+                  'name': t('header.home'),
+                  'url': 'https://karayaka.ru/',
+                },
+                ...navLinks.map((link) => ({
+                  '@type': 'SiteNavigationElement',
+                  'name': link.text,
+                  'url': `https://karayaka.ru${link.href}`,
+                })),
+              ],
+            }),
+          }}
+        />
+      </Head>
+      <header className={styles.header}>
+        <div
+          className={`${styles.overlay} ${isBurgerMenuOpen ? styles.active : ''}`}
+          onClick={() => setIsBurgerMenuOpen(false)}
+        ></div>
 
-      <nav className={`${styles.nav} ${styles.fill}`}>
-        <div className={styles.logo}>
-          <LinkWithLocale href="/">{t('header.home')}</LinkWithLocale>
-        </div>
-        <div className={styles.burgerButton} onClick={() => setIsBurgerMenuOpen(!isBurgerMenuOpen)}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <div className={`${styles.burgerMenu} ${isBurgerMenuOpen ? styles.open : ''}`}>
-          <button className={styles.closeButton} onClick={() => setIsBurgerMenuOpen(false)}>
-            <Icon path={mdiClose} size={1.5}></Icon>
-          </button>
-          <div className={styles.navLinks}>
-            <div className={styles.title}>
-              <div className={styles.logo}>
+        <nav className={`${styles.nav} ${styles.fill}`}>
+          <div className={styles.logo}>
+            <LinkWithLocale href="/">{t('header.home')}</LinkWithLocale>
+          </div>
+          <div className={styles.burgerButton} onClick={() => setIsBurgerMenuOpen(!isBurgerMenuOpen)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={`${styles.burgerMenu} ${isBurgerMenuOpen ? styles.open : ''}`}>
+            <button className={styles.closeButton} onClick={() => setIsBurgerMenuOpen(false)}>
+              <Icon path={mdiClose} size={1.5}></Icon>
+            </button>
+            <div className={styles.navLinks}>
+              <div className={styles.title}>
+                <div className={styles.logo}>
+                  <LinkWithLocale href="/" onClick={() => setIsBurgerMenuOpen(false)}>
+                    <Image src="/logo.png" alt="Logo" width={100} height={100} />
+                  </LinkWithLocale>
+                </div>
+              </div>
+
+              <div className={`${styles.navLink} ${styles.homeBurger} ${router.pathname === '/' ? styles.active : ''}`}>
                 <LinkWithLocale href="/" onClick={() => setIsBurgerMenuOpen(false)}>
-                  <Image src="/logo.png" alt="Logo" width={100} height={100} />
+                  {t('header.homeBurger')}
                 </LinkWithLocale>
               </div>
-            </div>
+              {navLinks.map((link, index) => (
+                <div key={index} className={`${styles.navLink} ${link.active ? styles.active : ''}`}>
+                  <LinkWithLocale href={link.href} onClick={() => setIsBurgerMenuOpen(false)}>
+                    {link.text}
+                  </LinkWithLocale>
+                </div>
+              ))}
 
-            <div className={`${styles.navLink} ${styles.homeBurger} ${router.pathname === '/' ? styles.active : ''}`}>
-              <LinkWithLocale href="/" onClick={() => setIsBurgerMenuOpen(false)}>
-                {t('header.homeBurger')}
-              </LinkWithLocale>
-            </div>
-            {navLinks.map((link, index) => (
-              <div key={index} className={`${styles.navLink} ${link.active ? styles.active : ''}`}>
-                <LinkWithLocale href={link.href} onClick={() => setIsBurgerMenuOpen(false)}>
-                  {link.text}
-                </LinkWithLocale>
+              <div className={styles.mobileLangSwitcher}>
+                <LanguageSwitcher lang="en">
+                  <button className={`${styles.mobileLangButton} ${currentLang === 'en' ? styles.active : ''}`}>
+                    English
+                  </button>
+                </LanguageSwitcher>
+                <LanguageSwitcher lang="ru">
+                  <button className={`${styles.mobileLangButton} ${currentLang === 'ru' ? styles.active : ''}`}>
+                    Русский
+                  </button>
+                </LanguageSwitcher>
               </div>
-            ))}
-
-            <div className={styles.mobileLangSwitcher}>
-              <LanguageSwitcher lang="en">
-                <button className={`${styles.mobileLangButton} ${currentLang === 'en' ? styles.active : ''}`}>
-                  English
-                </button>
-              </LanguageSwitcher>
-              <LanguageSwitcher lang="ru">
-                <button className={`${styles.mobileLangButton} ${currentLang === 'ru' ? styles.active : ''}`}>
-                  Русский
-                </button>
-              </LanguageSwitcher>
-            </div>
-            <div className={styles.contacts}>
-              <a href={contactInfo.whatsapp} target="_blank" rel="noopener noreferrer">
-                <Icon path={mdiWhatsapp} size={2} />
-              </a>
-              <a href={contactInfo.telegram} target="_blank" rel="noopener noreferrer">
-                <img src="/assets/icons/TelegramIconHeader.svg" alt="Telegram" className={styles.telegramIcon} />
-              </a>
+              <div className={styles.contacts}>
+                <a href={contactInfo.whatsapp} target="_blank" rel="noopener noreferrer">
+                  <Icon path={mdiWhatsapp} size={2} />
+                </a>
+                <a href={contactInfo.telegram} target="_blank" rel="noopener noreferrer">
+                  <img src="/assets/icons/TelegramIconHeader.svg" alt="Telegram" className={styles.telegramIcon} />
+                </a>
+              </div>
             </div>
           </div>
+        </nav>
+        <div className={styles.buttons}>
+          <div ref={langMenuRef} className={styles.dropdown} onClick={(e) => e.stopPropagation()}>
+            <button className={styles.button} onClick={() => setIsLanguageMenuOpen(!isLanguageMenuOpen)}>
+              {currentLang === 'en' ? 'English' : 'Русский'}
+              <Icon
+                path={mdiTriangleSmallDown}
+                size={1}
+                style={{
+                  transform: isLanguageMenuOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+                  transition: 'transform 0.5s ease',
+                }}
+              />
+            </button>
+            {isLanguageMenuOpen && (
+              <div className={styles.dropdownMenu}>
+                <LanguageSwitcher lang="en">
+                  <button
+                    className={`${styles.menuItem} ${currentLang === 'en' ? styles.active : ''}`}
+                    onClick={changeLanguage}
+                  >
+                    English
+                  </button>
+                </LanguageSwitcher>
+                <LanguageSwitcher lang="ru">
+                  <button
+                    className={`${styles.menuItem} ${currentLang === 'ru' ? styles.active : ''}`}
+                    onClick={changeLanguage}
+                  >
+                    Русский
+                  </button>
+                </LanguageSwitcher>
+              </div>
+            )}
+          </div>
         </div>
-      </nav>
-      <div className={styles.buttons}>
-        <div ref={langMenuRef} className={styles.dropdown} onClick={(e) => e.stopPropagation()}>
-          <button className={styles.button} onClick={() => setIsLanguageMenuOpen(!isLanguageMenuOpen)}>
-            {currentLang === 'en' ? 'English' : 'Русский'}
-            <Icon
-              path={mdiTriangleSmallDown}
-              size={1}
-              style={{
-                transform: isLanguageMenuOpen ? 'rotate(180deg)' : 'rotate(0deg)',
-                transition: 'transform 0.5s ease',
-              }}
-            />
-          </button>
-          {isLanguageMenuOpen && (
-            <div className={styles.dropdownMenu}>
-              <LanguageSwitcher lang="en">
-                <button
-                  className={`${styles.menuItem} ${currentLang === 'en' ? styles.active : ''}`}
-                  onClick={changeLanguage}
-                >
-                  English
-                </button>
-              </LanguageSwitcher>
-              <LanguageSwitcher lang="ru">
-                <button
-                  className={`${styles.menuItem} ${currentLang === 'ru' ? styles.active : ''}`}
-                  onClick={changeLanguage}
-                >
-                  Русский
-                </button>
-              </LanguageSwitcher>
-            </div>
-          )}
-        </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
