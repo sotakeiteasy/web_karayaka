c6522b7df563dae54b2c81ce58e58ae65f26e250
sotakeiteasy <murasov96@gmail.com>
2025-06-12 19:48:34 +0300

    feat: delete some inputs from contact-form

--- src/lib/components/contactUs/contactUs.tsx
+++ src/lib/components/contactUs/contactUs.tsx
@@ -19,38 +19,24 @@ export function ContactUs() {
     handleSubmit,
     setValue,
     reset,
-    watch,
     formState: { errors },
   } = useForm<Inputs>({
     defaultValues: {
       name: '',
-      surname: '',
       location: [],
       purpose: [],
-      city: '',
-      district: '',
-      budget: '',
       phone_number: '',
-      email: '',
       message: '',
     },
     mode: 'onBlur',
   });
 
-  const watchEmail = watch('email');
-  const watchPhone = watch('phone_number');
-
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     const templateParams = {
       name: data.name,
-      surname: data.surname,
-      email: data.email,
       phone_number: data.phone_number,
       location: data.location.join(', '),
       purpose: data.purpose.join(', '),
-      city: data.city || '',
-      district: data.district || '',
-      budget: data.budget || '',
       message: data.message || '',
     };
 
@@ -97,15 +83,6 @@ export function ContactUs() {
     });
   };
 
-  const validateContactInfo = (value: string, fieldName: 'email' | 'phone_number') => {
-    if (value && value.trim()) return true;
-
-    const otherField = fieldName === 'email' ? watchPhone : watchEmail;
-    if (otherField && otherField.trim()) return true;
-
-    return t('form.errors.contactRequired');
-  };
-
   return (
     <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       {isSubmitted ? (
@@ -118,101 +95,66 @@ export function ContactUs() {
           <legend className={styles.legend}>{t('form.title')}</legend>
           <p className={styles.description}>{t('form.description')}</p>
           <div className={styles.formRow}>
-            <div>
-              <label htmlFor="name" className={styles.label}>
-                {t('form.name')}
-              </label>
-              <input
-                type="text"
-                id="name"
-                className={errors.name ? styles.error : ''}
-                {...register('name', {
-                  required: t('form.errors.nameRequired'),
-                  maxLength: 20,
-                })}
-              />
-              {errors.name && <p className={styles.errorMessage}>{errors.name.message}</p>}
-              {!errors.name && <div className={styles.errorPlaceholder}></div>}
-            </div>
-
-            <div>
-              <label htmlFor="surname" className={styles.label}>
-                {t('form.surname')}
-              </label>
-              <input type="text" id="surname" {...register('surname', { maxLength: 20 })} />
-              <div className={styles.errorPlaceholder}></div>
-            </div>
+            <label htmlFor="name" className={styles.label}>
+              {t('form.name')}
+            </label>
+            <input
+              type="text"
+              id="name"
+              className={errors.name ? styles.error : ''}
+              {...register('name', {
+                required: t('form.errors.nameRequired'),
+                maxLength: 20,
+              })}
+            />
+            {errors.name && <p className={styles.errorMessage}>{errors.name.message}</p>}
+            {!errors.name && <div className={styles.errorPlaceholder}></div>}
           </div>
 
           <div className={styles.formDetails}>
-            <div className={styles.leftDetails}>
-              <fieldset className={styles.formLocation}>
-                <legend className={styles.buttonsLabel}>{t('form.location.label')}</legend>
-                <div>
-                  <input
-                    type="button"
-                    id="location-russia"
-                    name="location"
-                    value={t('form.location.russia')}
-                    className={locations.Russia ? `${styles.input} ${styles.selected}` : styles.input}
-                    onClick={() => toggleLocation(CountryType.Russia)}
-                  />
-                  <input
-                    type="button"
-                    id="location-turkey"
-                    name="location"
-                    value={t('form.location.turkey')}
-                    className={locations.Turkey ? `${styles.input} ${styles.selected}` : styles.input}
-                    onClick={() => toggleLocation(CountryType.Turkey)}
-                  />
-                </div>
-              </fieldset>
-
-              <fieldset className={styles.formPurpose}>
-                <legend className={styles.buttonsLabel}>{t('form.purpose.label')}</legend>
-                <div>
-                  <input
-                    type="button"
-                    id="purpose-buy"
-                    name="purpose"
-                    value={t('form.purpose.buy')}
-                    className={purposes.buy ? `${styles.input} ${styles.selected}` : styles.input}
-                    onClick={() => togglePurpose(SearchType.Buy)}
-                  />
-                  <input
-                    type="button"
-                    id="purpose-rent"
-                    name="purpose"
-                    value={t('form.purpose.rent')}
-                    className={purposes.rent ? `${styles.input} ${styles.selected}` : styles.input}
-                    onClick={() => togglePurpose(SearchType.Rent)}
-                  />
-                </div>
-              </fieldset>
-            </div>
-
-            <div className={styles.rightDetails}>
-              <div className={styles.formRow}>
-                <label htmlFor="city" className={styles.label}>
-                  {t('form.city')}
-                </label>
-                <input type="text" id="city" {...register('city', { maxLength: 15 })} />
+            <fieldset className={styles.formLocation}>
+              <legend className={styles.buttonsLabel}>{t('form.location.label')}</legend>
+              <div>
+                <input
+                  type="button"
+                  id="location-russia"
+                  name="location"
+                  value={t('form.location.russia')}
+                  className={locations.Russia ? `${styles.input} ${styles.selected}` : styles.input}
+                  onClick={() => toggleLocation(CountryType.Russia)}
+                />
+                <input
+                  type="button"
+                  id="location-turkey"
+                  name="location"
+                  value={t('form.location.turkey')}
+                  className={locations.Turkey ? `${styles.input} ${styles.selected}` : styles.input}
+                  onClick={() => toggleLocation(CountryType.Turkey)}
+                />
               </div>
+            </fieldset>
 
-              <div className={styles.formRow}>
-                <label htmlFor="district" className={styles.label}>
-                  {t('form.district')}
-                </label>
-                <input type="text" id="district" {...register('district', { maxLength: 15 })} />
+            <fieldset className={styles.formPurpose}>
+              <legend className={styles.buttonsLabel}>{t('form.purpose.label')}</legend>
+              <div>
+                <input
+                  type="button"
+                  id="purpose-buy"
+                  name="purpose"
+                  value={t('form.purpose.buy')}
+                  className={purposes.buy ? `${styles.input} ${styles.selected}` : styles.input}
+                  onClick={() => togglePurpose(SearchType.Buy)}
+                />
+                <input
+                  type="button"
+                  id="purpose-rent"
+                  name="purpose"
+                  value={t('form.purpose.rent')}
+                  className={purposes.rent ? `${styles.input} ${styles.selected}` : styles.input}
+                  onClick={() => togglePurpose(SearchType.Rent)}
+                />
               </div>
-
-              <div className={styles.formRow}>
-                <label htmlFor="budget" className={styles.label}>
-                  {t('form.budget')}
-                </label>
-                <input type="text" id="budget" {...register('budget', { maxLength: 15 })} />
-              </div>
-            </div>
+            </fieldset>
           </div>
 
           <div className={styles.formRow}>
@@ -224,7 +166,8 @@ export function ContactUs() {
               id="phone_number"
               className={errors.phone_number ? styles.error : ''}
               {...register('phone_number', {
-                validate: (value) => validateContactInfo(value, 'phone_number'),
+                required: t('form.errors.contactRequired'),
+
                 pattern: {
                   value: /^\+?[0-9]{7,15}$/,
                   message: t('form.errors.phoneInvalid'),
@@ -233,27 +176,6 @@ export function ContactUs() {
             />
             {errors.phone_number && <p className={styles.errorMessage}>{errors.phone_number.message}</p>}
             {!errors.phone_number && <div className={styles.errorPlaceholder}></div>}
-          </div>
-
-          <div className={styles.formRow}>
-            <label htmlFor="email" className={styles.label}>
-              {t('form.email')}
-            </label>
-            <input
-              type="email"
-              id="email"
-              autoComplete="email"
-              className={errors.email ? styles.error : ''}
-              {...register('email', {
-                validate: (value) => validateContactInfo(value, 'email'),
-                pattern: {
-                  value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
-                  message: t('form.errors.emailInvalid'),
-                },
-              })}
-            />
-            {errors.email && <p className={styles.errorMessage}>{errors.email.message}</p>}
-            {!errors.email && <div className={styles.errorPlaceholder}></div>}
           </div>
 
           <textarea
