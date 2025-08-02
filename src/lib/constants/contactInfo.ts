47a671f60b16129c3d79f3c8b39525f9695ca629
elen-afipe <190838117+elen-afipe@users.noreply.github.com>
2025-08-02 13:49:44 +0300

    upd: whatsapp number

--- src/lib/constants/contactInfo.ts
+++ src/lib/constants/contactInfo.ts
@@ -2,7 +2,7 @@ export const contactInfo = {
   phone: '+7 (919) 107-99-17',
   email: 'info@karayaka.ru',
   telegram: 'https://t.me/yselimmm',
-  whatsapp: 'https://wa.me/905320671890',
+  whatsapp: 'https://wa.me/905324694993',
   address: 'Г.МОСКВА, ВН.ТЕР.Г. МУНИЦИПАЛЬНЫЙ ОКРУГ НАГОРНЫЙ, УЛ БОЛОТНИКОВСКАЯ, Д. 7 К. 1, ПОМЕЩ. 3Ц',
   addressShort: 'Москва, ул. Болотниковская 7к1, помещение 3Ц',
   addressShortEn: 'Moscow, Bolotnikovskaya st., 7 bld. 1, room 3ts',
